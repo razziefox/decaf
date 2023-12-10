b12b5d09552f48ce01fd0b301a60509831d457af
florencio <florencio@razziefox.com>
2023-12-10 11:59:34 -0700

    miniaudio backend development started

--- audio_test.c
+++ audio_test.c
@@ -22,22 +22,65 @@ int mini_init(void) {
 
 }
 
+ma_sound *mini_load(char *audio_path) {
+
+    static ma_sound sound;
+
+    char exec_path[strlen(SDL_GetBasePath()) + 1];
+    strcpy(exec_path, SDL_GetBasePath());
+
+    char asset_path[256];
+
+    strcpy(asset_path, audio_path);
+
+    char file[strlen(exec_path) + strlen(asset_path) + 1];
+
+    strncat(exec_path, asset_path, 512);
+
+    strcpy(file, exec_path);
+
+    printf("audio loaded: %s\n", file);
+
+    ma_sound_init_from_file(&engine, file, 0, NULL, NULL, &sound);
+
+
+    return &sound;
+
+}
+
+void mini_play_file(ma_sound *sound) {
+    ma_result result;
+
+    result = ma_sound_start(sound);
+    if (result != MA_SUCCESS) {
+        printf("%d", result);
+    }
+
+}
+
 int mini_play(void) {
 
     char exec_path[strlen(SDL_GetBasePath()) + 1];
     strcpy(exec_path, SDL_GetBasePath());
-    char file_path[256];
 
-    strcpy(file_path, "/sound.wav");
+    char asset_path[256];
 
-    char file[strlen(exec_path) + strlen(file_path) + 1];
+    strcpy(asset_path, "assets/audio/test.wav");
 
-    strncat(exec_path, file_path, 512);
+    char file[strlen(exec_path) + strlen(asset_path) + 1];
 
-    ma_engine_play_sound(&engine, exec_path, NULL);
+    strncat(exec_path, asset_path, 512);
+
+    strcpy(file, exec_path);
+
+    ma_engine_play_sound(&engine, file, NULL);
     return 0;
 
 }
+
+void mini_stop(ma_sound *sound) { ma_sound_stop(sound); }
+
+bool mini_ended(ma_sound *sound) { return ma_sound_at_end(sound); }
 
 void mini_unload(void) {
 
--- audio_test.h
+++ audio_test.h
@@ -4,8 +4,14 @@
 #include "libs/miniaudio/miniaudio.h"
 #include "SDL.h"
 
+typedef ma_sound decaf_mini_sound;
+
 int mini_init(void);
 int mini_play(void);
 void mini_unload(void);
+ma_sound *mini_load(char *audio_path);
+void mini_play_file(ma_sound *sound);
+void mini_stop(ma_sound *sound);
+bool mini_ended(ma_sound *sound);
 
 #endif
